2beb36eaed15765893d8247ffd92280e4fe38512
tbdarch <toshack.desai@gmail.com>
2025-04-19 15:44:50 +0100

    deploy yml

--- src/main/java/com/example/CsvFileHandlerLambda.java
+++ src/main/java/com/example/CsvFileHandlerLambda.java
@@ -2,47 +2,47 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.s3.S3Client;
+
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import com.opencsv.CSVReaderBuilder;
-import java.io.InputStream;
 
 public class CsvFileHandlerLambda {
 
         private final SecretsManagerClient secretsClient;
         private final SnsClient snsClient;
         private final DynamoDbClient dynamoClient;
-        private final S3Client s3Client; // Add this line to declare the s3Client
-
-        // Constructor for test injection
-        public CsvFileHandlerLambda(SecretsManagerClient secretsClient, SnsClient snsClient,
-                        DynamoDbClient dynamoClient, S3Client s3Client) { // Updated constructor
-                this.secretsClient = secretsClient;
-                this.snsClient = snsClient;
-                this.dynamoClient = dynamoClient;
-                this.s3Client = s3Client; // Initialize s3Client
-        }
+        private final S3Client s3Client;
 
         // Default constructor for AWS Lambda runtime
         public CsvFileHandlerLambda() {
                 this(SecretsManagerClient.create(), SnsClient.create(), DynamoDbClient.create(), S3Client.create());
         }
 
+        // Constructor for testing
+        public CsvFileHandlerLambda(SecretsManagerClient secretsClient, SnsClient snsClient,
+                        DynamoDbClient dynamoClient, S3Client s3Client) {
+                this.secretsClient = secretsClient;
+                this.snsClient = snsClient;
+                this.dynamoClient = dynamoClient;
+                this.s3Client = s3Client;
+        }
+
         public void handleRequest(S3Event event, Context context) {
-                context.getLogger().log("Event: " + event.toString());
+                context.getLogger().log("Received event: " + event.toString());
 
                 String snsSecretName = System.getenv("SNS_SECRET_NAME");
                 String snsTopicArn = System.getenv("SNS_TOPIC_ARN");
@@ -51,71 +51,54 @@ public class CsvFileHandlerLambda {
                 try {
                         GetSecretValueResponse secretResponse = secretsClient.getSecretValue(
                                         GetSecretValueRequest.builder().secretId(snsSecretName).build());
-                        String emailEndpoint = secretResponse.secretString();
-                        context.getLogger().log("Email: " + emailEndpoint);
+                        String emailEndpoint = secretResponse.secretString(); // You can use this in SNS if needed
+                        context.getLogger().log("Retrieved secret email: " + emailEndpoint);
 
                         for (S3Event.S3EventNotificationRecord record : event.getRecords()) {
                                 String bucket = record.getS3().getBucket().getName();
                                 String key = record.getS3().getObject().getKey();
-                                context.getLogger().log("Bucket: " + bucket);
+                                context.getLogger().log("Processing S3 object: " + key + " from bucket: " + bucket);
 
-                                // Fetch file from S3
-                                try (S3ObjectInputStream s3ObjectInputStream = s3Client
-                                                .getObject(GetObjectRequest.builder().bucket(bucket).key(key).build())
-                                                .stream()) {
+                                // Download object from S3
+                                GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucket).key(key)
+                                                .build();
 
-                                        // Read CSV data
-                                        try (CSVReader reader = new CSVReaderBuilder(
-                                                        new InputStreamReader(s3ObjectInputStream)).build()) {
+                                try (InputStream inputStream = s3Client.getObject(getObjectRequest);
+                                                CSVReader reader = new CSVReaderBuilder(
+                                                                new InputStreamReader(inputStream)).build()) {
 
-                                                String[] header = reader.readNext(); // Read and process the header
-                                                context.getLogger().log("CSV Header: " + String.join(", ", header));
+                                        String[] headers = reader.readNext(); // Read header row
+                                        context.getLogger().log("CSV Headers: " + String.join(", ", headers));
 
-                                                String[] nextLine;
-                                                while ((nextLine = reader.readNext()) != null) {
-                                                        // Process each CSV row here
-                                                        Map<String, AttributeValue> item = new HashMap<>();
-                                                        item.put("employeeId", AttributeValue.builder().s(nextLine[0])
-                                                                        .build()); // Assuming first column is
-                                                                                   // employeeId
-                                                        item.put("firstName", AttributeValue.builder().s(nextLine[1])
-                                                                        .build()); // Assuming second column is
-                                                                                   // firstName
-                                                        item.put("middleName", AttributeValue.builder().s(nextLine[2])
-                                                                        .build()); // Assuming third column is
-                                                                                   // middleName
-                                                        item.put("lastName", AttributeValue.builder().s(nextLine[3])
-                                                                        .build()); // Assuming fourth column is lastName
-                                                        item.put("email", AttributeValue.builder().s(nextLine[4])
-                                                                        .build()); // Assuming fifth column is email
-                                                        item.put("documentName", AttributeValue.builder().s(nextLine[5])
-                                                                        .build()); // Assuming sixth column is
-                                                                                   // documentName
-                                                        item.put("externalStorage", AttributeValue.builder()
-                                                                        .s(nextLine[6]).build()); // Assuming seventh
-                                                                                                  // column is
-                                                                                                  // externalStorage
+                                        String[] nextLine;
+                                        while ((nextLine = reader.readNext()) != null) {
+                                                Map<String, AttributeValue> item = new HashMap<>();
+                                                item.put("employeeId", AttributeValue.builder().s(nextLine[0]).build());
+                                                item.put("firstName", AttributeValue.builder().s(nextLine[1]).build());
+                                                item.put("middleName", AttributeValue.builder().s(nextLine[2]).build());
+                                                item.put("lastName", AttributeValue.builder().s(nextLine[3]).build());
+                                                item.put("email", AttributeValue.builder().s(nextLine[4]).build());
+                                                item.put("documentName",
+                                                                AttributeValue.builder().s(nextLine[5]).build());
+                                                item.put("externalStorage",
+                                                                AttributeValue.builder().s(nextLine[6]).build());
 
-                                                        // Put item to DynamoDB
-                                                        dynamoClient.putItem(PutItemRequest.builder()
-                                                                        .tableName(dynamoTableName).item(item).build());
-                                                }
+                                                dynamoClient.putItem(PutItemRequest.builder().tableName(dynamoTableName)
+                                                                .item(item).build());
+
+                                                context.getLogger()
+                                                                .log("Inserted record into DynamoDB: " + nextLine[0]);
                                         }
-
-                                } catch (Exception e) {
-                                        context.getLogger()
-                                                        .log("Error reading or processing CSV file: " + e.getMessage());
-                                        e.printStackTrace();
                                 }
 
-                                // Send an SNS notification
-                                snsClient.publish(PublishRequest.builder().topicArn(snsTopicArn)
-                                                .message("CSV uploaded: s3://" + bucket + "/" + key)
-                                                .subject("CSV Upload").build());
-                                context.getLogger().log("Sent to SNS:");
+                                // Send SNS notification
+                                snsClient.publish(PublishRequest.builder().topicArn(snsTopicArn).subject("CSV Upload")
+                                                .message("CSV uploaded: s3://" + bucket + "/" + key).build());
+                                context.getLogger().log("Published SNS notification");
                         }
+
                 } catch (Exception e) {
-                        context.getLogger().log("Error processing CSV file: " + e.getMessage());
+                        context.getLogger().log("Error processing file: " + e.getMessage());
                         e.printStackTrace();
                 }
         }
